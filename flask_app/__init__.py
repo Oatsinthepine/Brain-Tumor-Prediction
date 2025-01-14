107dc372f034f8bec7463da8871950a5ef81ce96
Oatsinthepine <zzy20001201@gmail.com>
2025-01-14 18:32:52 +0800

    update the Render Postgre db conncection before deploy.

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-
+from flask_migrate import Migrate #get migrate here
 
 # initialise extensions
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
+migrate = Migrate() #create the Migrate object
+
 # this will redirect user to login page if not login
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
@@ -20,7 +22,9 @@ def create_app():
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
+    migrate.init_app(app, db) # Bind Flask-Migrate to the app and SQLAlchemy
 
+    # Register blueprints
     from flask_app.routes import main
     app.register_blueprint(main)
 
